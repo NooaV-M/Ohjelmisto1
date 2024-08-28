8424bde38aea3335412024c317d52c6f90fcb6f4
NooaV-M <nooa.vainio-mattila@metropolia.fi>
2024-08-28 16:24:55 +0300

    valmis mod 5

--- mod-05/Teht3.py
+++ mod-05/Teht3.py
@@ -2,11 +2,12 @@ num = (input("Enter a whole number or press enter to end program: "))
 
 while num != "":
 
-    for i in range(1,int(num)):
+    for i in range(2,(int(num))):
         if int(num) % i == 0:
-            print(f"{num} is a prime number")
+            print(f"{num} is not a prime number")
             break
     else:
-        print(f"{num} is not a prime number")
+        print(f"{num} is a prime number")
+
 
     num = (input("Enter a whole number or press enter to end program: "))
---
+++ mod-05/Teht4.py
@@ -0,0 +1,7 @@
+city_list =[]
+
+for i in range(5):
+    city_list.append(input("Please enter a city name: "))
+
+for i in city_list:
+    print(i)
